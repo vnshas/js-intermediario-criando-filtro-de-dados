a1e5061454985d9c3b51bdaa65b81cf0cd095947
vnshas <vnshas@gmail.com>
2024-12-06 14:40:09 -0300

    finalizado

--- script/script.js
+++ script/script.js
@@ -2,30 +2,39 @@
 
 //1) Crie uma função para filtrar os hotéis que estão localizados no Brasil
 function filterCountry(array) {
-  return;
+  const brazil = array.filter(array => array.country == "Brasil");
+  console.log(brazil)
 }
-console.log(filterCountry(data));
+filterCountry(data)
+
+
 
 // 2) Crie uma função para filtrar os hotéis que possuem o preço igual ou maior que R$200,00.
 function filterPrice(array) {
-  return;
+  const princeUnder200 = array.filter(array => array.price >= 200)
+  console.log(princeUnder200) 
 }
 console.log(filterPrice(data));
 
 //3) Crie uma função para filtrar os hotéis que estão abertos nesse momento (isOpen)
 function filterIsOpen(array) {
-  return;
+  const isOpen = array.filter(array => array.isOpen)
+  console.log(isOpen)
 }
-console.log(filterIsOpen(data));
+//console.log(filterIsOpen(data));
 
 //DESAFIO
 //4) Crie uma função que filtre apenas o hotel com o nome “Copacabana Palace”. Após isso, crie outra função para verificar as datas disponíveis para agendamento nesse hotel (isAvaliable).
 function filterHotelName(array) {
-  return;
+  return array.filter((hotel) => {
+    return hotel.name === "Copacabana Palace";
+  });
 }
 
 function filterToBook() {
-  const hotel = filterHotelName(data);
-  return;
+  const hotel = filterHotelName(data)[0];
+  return hotel.toBook.filter((bookingInfo) => {
+    return bookingInfo.isAvailable;
+  });
 }
 console.log(filterToBook());
